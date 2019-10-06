5de5ec1b8e1ef3a947057b58bdf84a4dc062ade3
Vishvang <vishvangp.8@gmail.com>
2019-10-06 17:40:33 -0400

    Add project files.

---
+++ EventManagementGig/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EventManagementGig
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
